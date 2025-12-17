f402a13aa78a881bb82bb1f78c3bb532928f0e77
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2025-12-17 14:32:35 +0700

    Practice: Different Ways to Use Print Function

---
+++ Basics/problem_1.py
@@ -0,0 +1,26 @@
+"""
+Write a program to print Twinkle twinkle little star poem in python.
+"""
+
+# This is the first way to print the poem
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!") 
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+
+# This is the second way to print the poem using a single print statement
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.""")
+
+# This is the third way to print the poem using triple quotes
+poem = """Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky."""
+
+print(poem)
+
+# This is the fourth way to print the poem using formatted string
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.")
