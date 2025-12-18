d3c76f3ed3e366f204df200edfc357b8a73c1b35
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2025-12-18 20:31:53 +0700

    Practice: Input function and type casting

---
+++ Basics/problem_3.py
@@ -0,0 +1,7 @@
+"""Write a python program to add two numbers."""
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+c = a + b
+print(f"The sum of {a} and {b} = {c}")
