844455845f3d2afda78fd614872df4ae2cc30ab5
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2025-12-21 22:56:01 +0700

    Practice: Strings Manipulation without any function

---
+++ Basics/Strings/problem_1.py
@@ -0,0 +1,16 @@
+"""
+Write a program to fill in a letter template given below with name and date. 
+letter = ''' Dear <|Name|>, 
+             You are selected! 
+             <|Date|> 
+'''
+
+"""
+name = input("Enter you name: ")
+date = input("Enter date (DD-MM-YYYY): ")
+
+letter = f"""Dear <|{name}|>,
+You are selected!
+<|{date}|>"""
+
+print(letter)
